9710d8b7e20a8d42186502a8c59ec4ae52519b66
nainalarahul <rahul.nainala@hexagon.com>
2023-01-16 15:28:25 +0530

    making changes to commit

--- src/components/LIMSService.js
+++ src/components/LIMSService.js
@@ -98,174 +98,171 @@ const LIMSService = () => {
           <div class="text_imp">
             <h2> Select any field you want to Overwrite</h2>
           </div>
-
            <div class="default"> 
-            <label htmlFor='Default'>Default &nbsp;&nbsp;</label>
+            <label htmlFor='Default'>Default </label><br/>
             <input type='text' id='Default' defaultValue= "Information" ref={DefaultRef} /><br />
             </div>
-
            <div class="default"> 
-            <label htmlFor='MicrosoftAspNetCore'>MicrosoftAspNetCore&nbsp;&nbsp;</label>
+            <label htmlFor='MicrosoftAspNetCore'>MicrosoftAspNetCore</label><br/>
             <input type='text' id='MicrosoftAspNetCore' defaultValue= "Warning" ref={MicrosoftAspNetCoreRef} /><br />
             </div>
-            
             <div class="default">  
-            <label htmlFor='CORSOrigin'>CORSOrigin&nbsp;&nbsp;</label>
+            <label htmlFor='CORSOrigin'>CORSOrigin</label><br/>
             <input type='text' id='CORSOrigin' defaultValue= "https://all the urls there are" ref={CorsOriginRef} /><br />
             </div>   
 
             <div class="default">  
-            <label htmlFor='AllowedHosts'>AllowedHosts&nbsp;&nbsp;</label>
+            <label htmlFor='AllowedHosts'>AllowedHosts</label><br/>
             <input type='text' id='AllowedHosts' defaultValue= "*" ref={AllowedHostsRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='Name'>Name&nbsp;&nbsp;</label>
+            <label htmlFor='Name'>Name</label><br/>
             <input type='text' id='Name' defaultValue= "Information" ref={NameRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='OpenApiTitle'>OpenApiTitle&nbsp;&nbsp;</label>
+            <label htmlFor='OpenApiTitle'>OpenApiTitle</label><br/>
             <input type='text' id='OpenApiTitle' defaultValue= "AIMAPI_NC_LIMSSerivceIntegration" ref={OpenApiTitleRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='OpenApiVersion'>OpenApiVersion&nbsp;&nbsp;</label>
+            <label htmlFor='OpenApiVersion'>OpenApiVersion</label><br/>
             <input type='text' id='OpenApiVersion' defaultValue= "1.0.0" ref={OpenApiVersionRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='TermsOfService'>TermsOfService&nbsp;&nbsp;</label>
+            <label htmlFor='TermsOfService'>TermsOfService</label><br/>
             <input type='text' id='TermsOfService' defaultValue= "http://ril.com" ref={TermsOfServiceRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='OpenApiDesc'>OpenApiDesc&nbsp;&nbsp;</label>
+            <label htmlFor='OpenApiDesc'>OpenApiDesc</label><br/>
             <input type='text' id='OpenApiDesc' defaultValue= "Api description" ref={OpenApiDescRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='OpenApiContactEmail'>OpenApiContactEmail&nbsp;&nbsp;</label>
+            <label htmlFor='OpenApiContactEmail'>OpenApiContactEmail</label><br/>
             <input type='text' id='OpenApiContactEmail' defaultValue= "ravi.ippili@ril.com" ref={OpenApiContactEmailRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='OpenApiContactName'>OpenApiContactName&nbsp;&nbsp;</label>
+            <label htmlFor='OpenApiContactName'>OpenApiContactName</label><br/>
             <input type='text' id='OpenApiContactName' defaultValue= "Ravi Ippili" ref={OpenApiContactNameRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='OpenApiContactUrl'>OpenApiContactUrl&nbsp;&nbsp;</label>
+            <label htmlFor='OpenApiContactUrl'>OpenApiContactUrl</label><br/>
             <input type='text' id='OpenApiContactUrl' defaultValue= "http://ril.com" ref={OpenApiContactUrlRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='OpenApiLicenseName'>OpenApiLicenseName&nbsp;&nbsp;</label>
+            <label htmlFor='OpenApiLicenseName'>OpenApiLicenseName</label><br/>
             <input type='text' id='OpenApiLicenseName' defaultValue= "Apache License Version 2.0" ref={OpenApiLicenseNameRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='OpenApiLicenseUrl'>OpenApiLicenseUrl&nbsp;&nbsp;</label>
+            <label htmlFor='OpenApiLicenseUrl'>OpenApiLicenseUrl</label><br/>
             <input type='text' id='OpenApiLicenseUrl' defaultValue= "http://www.apache.org/licenses/LICENSE_2.0" ref={OpenApiLicenseUrlRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='bizOwner'>bizOwner&nbsp;&nbsp;</label>
+            <label htmlFor='bizOwner'>bizOwner</label><br/>
             <input type='text' id='bizOwner' defaultValue= "Information" ref={BizOwnerRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='bizOwnerMail'>bizOwnerMail&nbsp;&nbsp;</label>
+            <label htmlFor='bizOwnerMail'>bizOwnerMail</label><br/>
             <input type='text' id='bizOwnerMail' defaultValue= "Information" ref={BizOwnerMailRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='endPointSecurityScheme'>endPointSecurityScheme&nbsp;&nbsp;</label>
+            <label htmlFor='endPointSecurityScheme'>endPointSecurityScheme</label><br/>
             <input type='text' id='endPointSecurityScheme' defaultValue= "Information" ref={EndPointSecuritySchemeRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='endPointSecurityAuthType'>endPointSecurityAuthType&nbsp;&nbsp;</label>
+            <label htmlFor='endPointSecurityAuthType'>endPointSecurityAuthType</label><br/>
             <input type='text' id='endPointSecurityAuthType' defaultValue= "Information" ref={EndPointSecurityAuthTypeRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='endPointSecurityUsername'>endPointSecurityUsername&nbsp;&nbsp;</label>
+            <label htmlFor='endPointSecurityUsername'>endPointSecurityUsername</label><br/>
             <input type='text' id='endPointSecurityUsername' defaultValue= "Information" ref={EndPointSecurityUsernameRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='tags'>tags&nbsp;&nbsp;</label>
+            <label htmlFor='tags'>tags</label><br/>
             <input type='text' id='tags' defaultValue="" ref={TagsRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='endPointSecurityPassword'>endPointSecurityPassword&nbsp;&nbsp;</label>
+            <label htmlFor='endPointSecurityPassword'>endPointSecurityPassword</label><br/>
             <input type='text' id='endPointSecurityPassword' defaultValue="" ref={EndPointSecurityPasswordRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='techOwner'>techOwner&nbsp;&nbsp;</label>
+            <label htmlFor='techOwner'>techOwner</label><br/>
             <input type='text' id='techOwner' defaultValue="" ref={TechOwnerRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='techOwnerMail'>techOwnerMail&nbsp;&nbsp;</label>
+            <label htmlFor='techOwnerMail'>techOwnerMail</label><br/>
             <input type='text' id='techOwnerMail' defaultValue="" ref={TechOwnerMailRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='tiersCollection'>tiersCollection&nbsp;&nbsp;</label>
+            <label htmlFor='tiersCollection'>tiersCollection</label><br/>
             <input type='text' id='tiersCollection' defaultValue="" ref={TiersCollectionRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='visibility'>visibility&nbsp;&nbsp;</label>
+            <label htmlFor='visibility'>visibility</label><br/>
             <input type='text' id='visibility' defaultValue="" ref={VisibilityRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='CORSAllowOrigins'>CORSAllowOrigins&nbsp;&nbsp;</label>
+            <label htmlFor='CORSAllowOrigins'>CORSAllowOrigins</label><br/>
             <input type='text' id='CORSAllowOrigins' defaultValue="" ref={CorsAllowOriginsRef} /><br />   
             </div>
 
             <div class="default">  
-            <label htmlFor='CorsAllowHeaders'>CorsAllowHeaders&nbsp;&nbsp;</label>
+            <label htmlFor='CorsAllowHeaders'>CorsAllowHeaders</label><br/>
             <input type='text' id='CorsAllowHeaders' defaultValue="" ref={CorsAllowHeadersRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='Login'>Login&nbsp;&nbsp;</label>
+            <label htmlFor='Login'>Login</label><br/>
             <input type='text' id='Login' defaultValue="" ref={LoginRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='PostLims'>PostLims&nbsp;&nbsp;</label>
+            <label htmlFor='PostLims'>PostLims</label><br/>
             <input type='text' id='PostLims' defaultValue="" ref={PostLimsRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='sslCallback'>sslCallback&nbsp;&nbsp;</label>
+            <label htmlFor='sslCallback'>sslCallback</label><br/>
             <input type='text' id='sslCallback' defaultValue="" ref={SslCallbackRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='grant_type'>grant_type&nbsp;&nbsp;</label>
+            <label htmlFor='grant_type'>grant_type</label><br/>
             <input type='text' id='grant_type' defaultValue="" ref={GranttypeRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='clientId'>clientId&nbsp;&nbsp;</label>
+            <label htmlFor='clientId'>clientId</label><br/>
             <input type='text' id='clientId' defaultValue="" ref={ClientIdRef} /><br />
             </div>
 
             <div class="default">  
-            <label htmlFor='clientSecret'>clientSecret&nbsp;&nbsp;</label>
+            <label htmlFor='clientSecret'>clientSecret</label><br/>
             <input type='text' id='clientSecret' defaultValue="" ref={ClientSecretRef} /><br />
             </div>
             
             <div class="default">  
-            <label htmlFor='scope'>scope&nbsp;&nbsp;</label>
+            <label htmlFor='scope'>scope</label><br/>
             <input type='text' id='scope' defaultValue="" ref={ScopeRef} /><br />
             </div>
             <br/>
