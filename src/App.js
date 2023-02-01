a99727992cbc9cd515305e7929e73c465b69caa0
nainalarahul <rahul.nainala@hexagon.com>
2023-02-01 10:29:31 +0530

    updated code as of 01 feb 2023

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import AzureConnection from './components/backend/azureconnection';
+// import CheckConn from './components/backend/checkconn';
 
 function App() {
   return (
--- src/components/backend/azureconnection.js
+++ src/components/backend/azureconnection.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { Button, Text, View } from 'react-native';
 import axios from 'axios';
 
-const PAT = 'rzuux4bfyknoc247asdqwm7pxxrmwpth2a5nyv7fnkmr5fknoq6a';
+// const PAT = 'rzuux4bfyknoc247asdqwm7pxxrmwpth2a5nyv7fnkmr5fknoq6a';
 const AzureConnection = () => {
   const [message, setMessage] = useState('');
   const handleClick = async () => {
@@ -13,13 +13,12 @@ const AzureConnection = () => {
 
       const headers = {
         'Content-Type': 'application/json',
-        // 'Authorization': 'Basic dXNlcm5hbWU6cnp1dXg0YmZ5a25vYzI0N2FzZHF3bTdweHhybXdwdGgyYTVueXY3Zm5rbXI1Zmtub3E2YQ==',
-        'Authorization': `Bearer ${PAT}`,
+        'Authorization': 'Basic OnE1Z3lvY3psZDdoendvNGhlNDJoZ3R5N3NleGZiZWV2N2t3NGl1dXNmMzZvYngzc3FjYWE=',
+        // 'Authorization': `Bearer ${PAT}`,
 
 
       }
        
-      
       console.log('Headers:', headers);
       const result = await axios.post(
         projectPath,
@@ -46,8 +45,8 @@ const AzureConnection = () => {
         },
         { headers }
       );
-      // This code is making an HTTP POST request to the URL specified by projectPath with a JSON payload that includes information about updates to a Git repository. The payload includes an array of refUpdates, with a single update to the Git reference refs/heads/main to change its object ID to the specified COMMIT_HASH. The payload also includes an array of commits, with a single commit that includes a comment "New File created" and a change to add a file at the specified path. The request also includes headers as additional request headers.
 
+      console.log("data reached");
       setMessage(`File pushed successfully with push ID: ${result.data.commitId}`);
     } catch (error) {
       setMessage(`Error: ${error.message}`);
@@ -64,18 +63,3 @@ const AzureConnection = () => {
 
 export default AzureConnection;
 
-
-
-// import axios from 'axios';
-
-// const getData = async () => {
-//   const accessToken = await getAccessTokenFromAAD(); // implement this function to get the access token from AAD
-//   const headers = {
-//     'Content-Type': 'application/json',
-//     'Authorization': `Bearer ${accessToken}`,
-//   };
-
-//   const response = await axios.get('https://dev.azure.com/{organization}/{project}/_apis/build/builds?api-version=6.0', { headers });
-
-//   console.log(response.data);
-// };
